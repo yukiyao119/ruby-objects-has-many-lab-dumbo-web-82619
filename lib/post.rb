2fa2d8ccaa570a1800fa26fd08adbb41983d296b
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-29 03:55:10 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,29 @@
+class Post 
+  
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    @@all << self
+    # save  alternertive is def save method
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def author_name
+    if self.author != nil 
+      self.author.name 
+    else 
+      nil 
+    end
+  end 
+  
+end
